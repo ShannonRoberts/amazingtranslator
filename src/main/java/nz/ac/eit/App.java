a0f14d574a7e56ae1abee3a4d51be3a96ac33972
Shannon Roberts <sneakypuppers@gmail.com>
2019-08-20 14:57:17 +1200

    asked for a number to translate and display the translated number

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -1,5 +1,7 @@
 package nz.ac.eit;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! changed" );
+        System.out.println( "What is the number to translate?" );
+        Scanner sc = new Scanner(System.in);
+        String userNumber = sc.nextLine();
+        int translatedNumber = Integer.parseInt(userNumber);
+        System.out.println("translated number " + translatedNumber);
     }
 }
